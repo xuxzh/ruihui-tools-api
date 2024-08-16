6c27610c3cd6691fe02c3e5935011780dcb2d3fa
xu_xz <xuxiaozhong@cptgroup.cn>
2024-08-16 09:05:25 +0800

    opt: 优化aac_user接口

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,27 +3,42 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
+const select = {
+  id: true,
+  userCode: true,
+  userName: true,
+  email: true,
+  mobile: true,
+  telephone: true,
+  remark: true,
+  updatedAt: true,
+  createdAt: true,
+};
+
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {
     //
   }
   create(createUserDto: CreateUserDto) {
-    return this.prisma.aac_user.create({ data: createUserDto });
+    return this.prisma.aac_user.create({ data: createUserDto, select });
   }
 
   findAll() {
-    return this.prisma.aac_user.findMany();
+    return this.prisma.aac_user.findMany({
+      select,
+    });
   }
 
   findOne(id: number) {
-    return this.prisma.aac_user.findUnique({ where: { id } });
+    return this.prisma.aac_user.findUnique({ where: { id }, select });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return this.prisma.aac_user.update({
       where: { id },
       data: updateUserDto,
+      select,
     });
   }
 
