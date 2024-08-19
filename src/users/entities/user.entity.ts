8faa5b95f52ce546253e02013957cb11db89ecd3
xu_xz <xuxiaozhong@cptgroup.cn>
2024-08-19 20:38:11 +0800

    opt: 添加user响应类型

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,30 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { aac_user } from '@prisma/client';
+export class UserEntity implements aac_user {
+  @ApiProperty()
+  id: number;
+  @ApiProperty()
+  userCode: string;
+  @ApiProperty()
+  userName: string;
+  @ApiProperty()
+  password: string;
+  @ApiProperty()
+  salt: string;
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  mobile: string;
+  @ApiProperty()
+  telephone: string;
+  @ApiProperty()
+  remark: string;
+  @ApiProperty()
+  deleted: boolean;
+  @ApiProperty()
+  deletedAt: Date;
+  @ApiProperty()
+  createdAt: Date;
+  @ApiProperty()
+  updatedAt: Date;
+}
